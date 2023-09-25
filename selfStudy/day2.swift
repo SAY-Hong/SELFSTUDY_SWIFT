bfdfd7baf96e7ea86f9a730a01a913cc7b527221
SAY-Hong <bobseahee@gmail.com>
2023-09-25 10:42:47 +0900

    guard-let 수정하기

--- selfStudy/day2.swift
+++ selfStudy/day2.swift
@@ -9,17 +9,11 @@
 var money: Int = 0
 var menuNumber: Int = 0
 
-func input(message: String) -> Int? {
+func input(message: String) -> Int {
     print(message, terminator: "")
     guard let inputValue = readLine(), let inputValue = Int(inputValue) else {
         print("입력값이 잘못되었습니다.")
-        return nil
+        return input(message: message)
     }
     return inputValue
 }
-
-/*money = input(message: "금액 입력: ") ?? money
-menuNumber = input(message: """
-메뉴를 고르세요
-1. 콜라(800원) 2. 생수(500원) 3.비타민워터(1500원):
-""") ?? menuNumber*/
--- selfStudy/main.swift
+++ selfStudy/main.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 //while 예제7
-let userSum: Int = input(message: "예상되는 두 숫자의 합을 작성하시오: ") ?? userSum
+let userSum: Int = input(message: "예상되는 두 숫자의 합을 작성하시오: ")
 let number1 = Int.random(in: 0...99)
 let number2 = Int.random(in: 0...99)
 
@@ -19,28 +19,29 @@ if userSum == number1 + number2 {
     print("Fail")
 }
 
-//while 예제8
-var flag: Bool = true
-print("==plus Game==")
-while flag {
-    print("예상되는 두 숫자의 합을 작성하시오: ", terminator: "")
-    let userSum = Int(readLine()!)!
-    
-    var number1 = Int.random(in: 0...99)
-    var number2 = Int.random(in: 0...99)
-    
-    print("\(number1) + \(number2) = \(userSum)")
-    if userSum == number1 + number2 {
-        print("Success")
-        print("계속 하시겠습니까? (Y/N):", terminator: "")
-        let yesOrNo = readLine()!
-        
-        if yesOrNo == "N" {
-            flag = false
-        }
-    } else {
-        print("Fail")
-        flag = false
-    }
-}
-
+////while 예제8
+//var flag: Bool = true
+//print("==plus Game==")
+//
+//while flag {
+//    print("예상되는 두 숫자의 합을 작성하시오: ", terminator: "")
+//    let userSum = Int(readLine()!)!
+//
+//    var number1 = Int.random(in: 0...99)
+//    var number2 = Int.random(in: 0...99)
+//
+//    print("\(number1) + \(number2) = \(userSum)")
+//    if userSum == number1 + number2 {
+//        print("Success")
+//        print("계속 하시겠습니까? (Y/N):", terminator: "")
+//        let yesOrNo = readLine()!
+//
+//        if yesOrNo == "N" {
+//            flag = false
+//        }
+//    } else {
+//        print("Fail")
+//        flag = false
+//    }
+//}
+//
