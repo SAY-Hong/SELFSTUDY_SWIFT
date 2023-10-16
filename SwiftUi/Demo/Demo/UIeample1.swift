8b8d0a716fe2f964df61c2865e16ccd0d0e0229d
SAY-Hong <bobseahee@gmail.com>
2023-10-16 14:48:32 +0900

    SwiftUI 눕혔을 때 나오게 하기
    offset으로 버튼을 고정시키니까 가로로 눕혔을 때 안나오는 것이었다! -> Spacer()로 간격 조절

--- SwiftUi/Demo/Demo/UIeample1.swift
+++ SwiftUi/Demo/Demo/UIeample1.swift
@@ -11,14 +11,18 @@ struct UIeample1: View {
     @State private var count = 0
     
     var body: some View {
-        Text("\(count)")
-        Button(action: { plusCount() }, label: {
-            Text("Count")
-                .padding(.leading, 150)
-                .padding(.trailing, 150)
-        }).offset(y: 300)
-          .buttonStyle(.borderedProminent)
-          .tint(.purple)
+        VStack {
+            Spacer()
+            Text("\(count)")
+        Spacer()
+            Button(action: { plusCount() }, label: {
+                Text("Count")
+                    .padding(.leading, 150)
+                    .padding(.trailing, 150)
+            })
+              .buttonStyle(.borderedProminent)
+              .tint(.purple)
+        }
     }
     func plusCount() {
         count += 1
---
+++ SwiftUi/Demo/UIexample3.swift
@@ -0,0 +1,18 @@
+//
+//  UIexample3.swift
+//  Demo
+//
+//  Created by 홍세희 on 2023/10/16.
+//
+
+import SwiftUI
+
+struct UIexample3: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UIexample3()
+}
