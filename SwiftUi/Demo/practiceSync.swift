d8e130c55816a6952cabd7640316bbfc616431e3
SAY-Hong <bobseahee@gmail.com>
2023-10-19 17:52:23 +0900

    비동기 코멘트

--- SwiftUi/Demo/practiceSync.swift
+++ SwiftUi/Demo/practiceSync.swift
@@ -24,11 +24,11 @@ struct practiceSync: View {
     
     func doSomething() async {
         // async let을 사용하여 두 개의 비동기 작업을 동시에 실행
-        async let result1 = sumFrom(start: 1, end: 50)
+        async let result1 = sumFrom(start: 1, end: 50) //스레드야! 나 바쁘면 다른 스레드에 넘길거다? 그리고 다른 함수 오면 실행한다?? 
         async let result2 = sumFrom(start: 51, end: 100)
         // 결과를 출력
-        var a = await result1
-        var b = await result2
+        var a = await result1 //sumFrom이 실행돼서 result1을 반환할 때까지 실행 안할거야!
+        var b = await result2 //sumFrom이 실행돼서 result2를 반환할 때까지 실행 안할거야!
         print("\(a + b)")
     }
 
