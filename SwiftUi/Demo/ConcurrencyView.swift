fd769849228a564906ad303a59aea6e5d615ddb2
SAY-Hong <bobseahee@gmail.com>
2023-10-20 01:00:47 +0900

    비동기 - withTaskGroup
    1. 캡처리스트의 존재
    2. withTaskGroup(of: Int.self) -> of 뒤에는 태스크그룹이 리턴하는 값을 써주기

---
+++ SwiftUi/Demo/ConcurrencyView.swift
@@ -0,0 +1,51 @@
+//
+//  withTaskGroup.swift
+//  Demo
+//
+//  Created by 홍세희 on 2023/10/19.
+//
+
+import SwiftUI
+
+struct ConcurrencyView: View {
+    var body: some View {
+        Button(action: {
+            Task {
+                await print(doSomething())
+            }
+        }, label: {
+            Text("Do Something")
+        })
+    }
+
+    func doSomething() async -> Int {
+        var total = 0
+        var indexList = [1, 50]
+        await withTaskGroup(of: Int.self) { [indexList] group in //'[indexList]': 캡처리스트 -> 교착상태 예방
+            for i in 0..<indexList.count {
+                group.addTask {
+                   return await sumFrom(indexList[i] + 1, indexList[i] + 50)
+                }
+            }
+            var totalSum: [Int] = []
+            
+            for await total in group {
+                totalSum.append(total)
+            }
+            total = totalSum.reduce(0, +)
+        }
+        return total
+    }
+
+    func sumFrom(_ s: Int, _ e: Int) async -> Int {
+        var sum = 0
+        for i in s...e {
+            sum += i
+        }
+        return sum
+    }
+}
+
+#Preview {
+    ConcurrencyView()
+}
