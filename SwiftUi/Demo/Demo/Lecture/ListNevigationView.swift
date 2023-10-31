e343ac57b797f1cf53d6b7513b2f307f34841500
SAY-Hong <bobseahee@gmail.com>
2023-10-31 16:17:14 +0900

    SwiftUI 교재 p.300까지

---
+++ SwiftUi/Demo/Demo/Lecture/ListNevigationView.swift
@@ -0,0 +1,18 @@
+//
+//  ListNevigationView.swift
+//  Demo
+//
+//  Created by 홍세희 on 2023/10/31.
+//
+
+import SwiftUI
+
+struct ListNevigationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListNevigationView()
+}
