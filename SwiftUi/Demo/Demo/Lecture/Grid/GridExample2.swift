4efa39b2d99c47bd0e8cb644babaa0ff19e28ce5
SAY-Hong <bobseahee@gmail.com>
2023-11-09 00:33:27 +0900

    SwiftUI 그리드 예제
    Slider 대신 버튼으로 시험해보기

---
+++ SwiftUi/Demo/Demo/Lecture/Grid/GridExample2.swift
@@ -0,0 +1,18 @@
+//
+//  GridExample2.swift
+//  Demo
+//
+//  Created by 홍세희 on 2023/11/09.
+//
+
+import SwiftUI
+
+struct GridExample2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GridExample2()
+}
